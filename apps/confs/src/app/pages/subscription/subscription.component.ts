7c2c5aac692badb10d214b7079ab451849e86a7a
TiagoTNX <tiagotnx@gmail.com>
2022-07-30 20:10:43 -0300

    :speech_balloon: text correction

--- apps/confs/src/app/pages/subscription/subscription.component.ts
+++ apps/confs/src/app/pages/subscription/subscription.component.ts
@@ -10,18 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 export class SubscriptionComponent implements OnInit {
   url!: string;
   constructor(public authService: AuthService, private route: ActivatedRoute) {
+    const { id } = this.route.snapshot.params;
+    const text =
+      'Fala, dev! participarei de um evento presencial organizado pelo DevPr, vamos fazer network e aprender novas tecnologias.\n\nJunte-se a mim pelo link:';
+    const eventUrl = `devpr.org/${id}`;
+
     if ('id' in this.route.snapshot.params) {
-      const { id } = this.route.snapshot.params;
-      const eventUrl = `https://github.com/${id}`;
-      const text =
-        'Fala, dev! participarei de um evento presencial organizado pelo DevPr, vamos fazer network e aprender novas tecnologias.\n\nJunte-se a mim pelo link:';
       this.url = `http://twitter.com/share?text=${text}&url=${eventUrl}&hashtags=DevPrConf&via=DevPr`;
       this.authService.loadUserUrl(id);
     }
 
     this.authService.githubUser$.subscribe((user) => {
       if (user.login) {
-        this.url = `http://twitter.com/share?text=Testando o compartilhamento&url=https://github.com/${user.login}&hashtags=DevPrConf&via=devpr`;
+        this.url = `http://twitter.com/share?text=${text}&url=devpr.org/${user.login}&hashtags=DevPrConf&via=DevPr`;
       }
     });
   }
