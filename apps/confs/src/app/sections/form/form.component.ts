525d2a467401935140ac571c92206441bb87d99b
TiagoTNX <tiagotnx@gmail.com>
2022-08-13 00:47:35 -0300

    :recycle: Component form

---
+++ apps/confs/src/app/sections/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'confs-form, form[confs-form]',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- apps/confs/src/app/sections/sections.module.ts
+++ apps/confs/src/app/sections/sections.module.ts
@@ -4,14 +4,16 @@ import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthModule } from '../shared/auth/auth.module';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     BannerComponent,
     FooterComponent,
+    FormComponent,
   ],
   imports: [CommonModule, AuthModule],
-  exports: [HeaderComponent, BannerComponent, FooterComponent],
+  exports: [HeaderComponent, BannerComponent, FooterComponent, FormComponent],
 })
 export class SectionsModule {}
