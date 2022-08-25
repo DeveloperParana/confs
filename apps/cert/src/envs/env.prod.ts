f7d8155e95f5b757591fb7510621c436df9fd19a
Guilherme Siquinelli <guiseek@gmail.com>
2022-08-25 17:36:55 -0300

    feat: cert app (#12)

---
+++ apps/cert/src/envs/env.prod.ts
@@ -0,0 +1,4 @@
+export const env = {
+  production: true,
+  'server.api': 'https://api.devpr.org',
+};
--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Render, Get, Param } from '@nestjs/common';
-import { existsSync } from 'fs';
-import { environment } from '../environments/environment';
+import { Controller, Get } from '@nestjs/common';
+import { map } from 'rxjs';
 
 import { AppService } from './app.service';
 
@@ -8,40 +7,55 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index.hbs')
-  getDevParana() {
-    return this.appService.getGithubUser('developerparana');
+  @Get('members')
+  members() {
+    return this.appService.getMembers().pipe(
+      map(({ data }) => {
+        return data.map((m) => ({
+          status: m.status,
+          photo: m.photo,
+          city: m.city,
+          name: m.name,
+          id: m.id,
+        }));
+      })
+    );
   }
+  // @Get()
+  // @Render('index.hbs')
+  // getDevParana() {
+  //   return this.appService.getGithubUser('developerparana');
+  // }
 
-  @Get(':username')
-  @Render('index.hbs')
-  getData(@Param('username') username: string) {
-    return this.appService.getGithubUser(username);
-  }
+  // @Get(':username')
+  // @Render('index.hbs')
+  // getData(@Param('username') username: string) {
+  //   return this.appService.getGithubUser(username);
+  // }
 
-  @Get('ticket/:username')
-  @Render('ticket.hbs')
-  ticketUser(@Param('username') username: string) {
-    if (!existsSync(`assets/${username}.webp`)) {
-      const url = environment['server.api'];
-      return { url: `${url}/${username}/ticket` };
-    }
-    return this.appService.getGithubUser(username);
-  }
+  // @Get('ticket/:username')
+  // @Render('ticket.hbs')
+  // ticketUser(@Param('username') username: string) {
+  //   if (!existsSync(`assets/${username}.webp`)) {
+  //     const url = environment['server.api'];
+  //     return { url: `${url}/${username}/ticket` };
+  //   }
+  //   return this.appService.getGithubUser(username);
+  // }
 
-  @Get('webp/:username')
-  @Render('webp.hbs')
-  webpUser(@Param('username') username: string) {
-    return { path: `assets/${username}.webp` };
-  }
+  // @Get('webp/:username')
+  // @Render('webp.hbs')
+  // webpUser(@Param('username') username: string) {
+  //   return { path: `assets/${username}.webp` };
+  // }
 
-  @Get(':username/ticket')
-  userTicket(@Param('username') username: string) {
-    if (existsSync(`assets/${username}.webp`)) {
-      const url = environment['server.api'];
-      return { url: `${url}/ticket/${username}` };
-    }
-    return this.appService.createTicket(username);
-  }
+  // @Get(':username/ticket')
+  // userTicket(@Param('username') username: string) {
+  //   if (existsSync(`assets/${username}.webp`)) {
+  //     const url = environment['server.api'];
+  //     return { url: `${url}/ticket/${username}` };
+  //   }
+  //   return this.appService.createTicket(username);
+  // }
+
 }
