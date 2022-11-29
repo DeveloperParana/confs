102b2c6263381571d5ca59882b3daed608ce7c39
Gui Seek <guiseek@gmail.com>
2022-11-28 21:35:13 -0300

    style: 💄 remove console.logs

--- libs/event/feature-shell/src/lib/event-feature-shell.component.ts
+++ libs/event/feature-shell/src/lib/event-feature-shell.component.ts
@@ -44,8 +44,6 @@ export class EventFeatureShellComponent {
     @Inject('event.date') readonly eventDate: string,
     @Inject('pages') readonly pages: { project: number }
   ) {
-    console.log(new Date(this.eventDate));
-
     this.date = new Date(eventDate);
     this.year = this.date.getFullYear().toString();
     this.projectFacade.loadProjectColumns(pages.project);
