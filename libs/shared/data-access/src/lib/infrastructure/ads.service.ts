22e49f0061f492781a59f204521b6d0429b50170
Guilherme Siquinelli <guiseek@gmail.com>
2022-12-20 05:12:57 -0300

    feat: 🎸 twitter pixel (#56)

---
+++ libs/shared/data-access/src/lib/infrastructure/ads.service.ts
@@ -0,0 +1,16 @@
+declare const twq: (...params: unknown[]) => void;
+
+export class AdsService {
+  events = {
+    c4p: () =>
+      twq('event', 'tw-od9na-od9nc', {
+        /** event id */
+        status: 'started'
+      }),
+    ticket: () => () => 'ticket',
+  };
+
+  dispatchEvent<K extends keyof AdsService['events']>(event: K) {
+    return this.events[event].apply(this);
+  }
+}
